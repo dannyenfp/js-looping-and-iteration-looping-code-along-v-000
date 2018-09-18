55486912b6b00b4d274b9e7deee219e70927a49f
Daniel Valmas <daniel.valmas@gmail.com>
2018-09-18 18:22:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const gifts = ["teddy bear", "drone", "doll"];
 function wrapGift(gift) {
   console.log(`Wrapped ${gift} and added a bow!`);
 }
+
+wrapGift(gifts[0]);
+wrapGift(gifts[1]);
+wrapGift(gifts[2]);
