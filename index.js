f39a09cb2490260cfac3bd60d9852380966a1dfc
Daniel Valmas <daniel.valmas@gmail.com>
2018-09-18 18:29:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,17 @@ function printBadges(arr){
   }
   return arr;
 }
+
+function maybeTrue() { 
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails(){ 
+  let tails = 0; 
+  
+  while (maybeTrue()) { 
+    tails++;
+  }
+  
+  return `You got ${tails} tails in a row!`; 
+}
