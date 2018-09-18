8c6e97bbbc418ff4ef953515c3a8d46cb6407abc
Daniel Valmas <daniel.valmas@gmail.com>
2018-09-18 18:25:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ wrapGift(gifts[2]);
 
 function printBadges(arr){ 
   for (let i = 0; i < arr.length; i++) {
-    console.log(`Welcome ${arr[i]}! You are employee #${i+1}.);
+    console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`);
   }
   return arr;
 }
